d2fb659de132bab586f31033d796e170847fae6d
anand <anandbs291@gmail.com>
2026-09-11 23:03:14 +0530

    fix

--- apps/mobile/src/services/index.js
+++ apps/mobile/src/services/index.js
@@ -3,4 +3,5 @@ export {encryptBackupPayload,decryptBackupPayload} from './backupCrypto.js';
 export {isBiometrySupported,enableBiometricUnlock,disableBiometricUnlock,verifyBiometricUnlock} from './appLock.js';
 export {default as RNFS} from 'react-native-fs';
 export {default as Share} from 'react-native-share';
-export * as XLSX from 'xlsx';
+import * as XLSX from 'xlsx';
+export {XLSX};
