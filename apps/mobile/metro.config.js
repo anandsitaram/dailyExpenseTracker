dc9a3aacc38e49b84351b665d3b1fcba1d41b347
anand <anandbs291@gmail.com>
2026-09-11 22:18:52 +0530

    fix

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
 const config = {
-  watchFolders: [path.resolve(__dirname, '../../packages/core')],
+  watchFolders: [
+    path.resolve(workspaceRoot, 'packages/core'),
+  ],
+
   resolver: {
     unstable_enableSymlinks: false,
+
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
   },
 };
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+module.exports = mergeConfig(
+  getDefaultConfig(projectRoot),
+  config
+);
